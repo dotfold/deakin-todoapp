7dba0bafd285b8fb367608f4375e034d85c684ac
dotfold <james@dotfold.io>
2018-08-03 15:51:46 -0700

    feat(app): entrypoint creates a navigation stack

--- App.js
+++ App.js
@@ -1,44 +1,14 @@
-import React from 'react'
-import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import { Home } from './app/home'
+import { List } from './app/list'
 
-import { Item } from './app/item'
-import { Card } from './app/card'
+// load data from AsyncStorage
 
-export default class App extends React.Component {
-  render () {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Welcome Home!</Text>
-        <Card>
-          <Item title='First Active Card Title' />
-        </Card>
-      </View>
-    )
-  }
-}
+// then move to home
+import { createStackNavigator } from 'react-navigation'
 
-// TODO: use React Context API and have a Dimensions provider
-// that propagates new dimensions from a change event
-const { height, width } = Dimensions.get('window')
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start'
-  },
-  title: {
-    fontSize: 36,
-    marginTop: 60,
-    marginBottom: 30,
-    fontWeight: '300'
-  },
-  card: {
-    backgroundColor: '#ccc',
-    padding: 20,
-    // flex: 1,
-    width: width - 25,
-    height: 185,
-    borderRadius: 10
-  }
+const App = createStackNavigator({
+  Home: { screen: Home },
+  List: { screen: List }
 })
+
+export default App
