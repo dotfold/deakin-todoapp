5540f902017228a6ba722310d9406eb6dfe470a4
dotfold <james@dotfold.io>
2018-08-10 00:30:23 -0700

    feat(app): switch list screen to the navigator

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { Home } from './app/home'
 import { List } from './app/list'
+import { ListNavigator } from './app/list_navigator'
 
 // load data from AsyncStorage
 
@@ -8,7 +9,7 @@ import { createStackNavigator } from 'react-navigation'
 
 const App = createStackNavigator({
   Home: { screen: Home },
-  List: { screen: List }
+  List: { screen: ListNavigator }
 })
 
 export default App
