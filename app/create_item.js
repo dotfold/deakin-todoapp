04844d7ad3831ce41a47464408f29273b3602b6d
dotfold <james@dotfold.io>
2018-08-11 00:15:08 -0700

    feat(create_item): toggle control to tag the current geo position with the item

--- app/create_item.js
+++ app/create_item.js
@@ -1,19 +1,33 @@
 import React from 'react'
-import { StyleSheet, TextInput } from 'react-native'
+import { Switch, View, StyleSheet, Text, TextInput } from 'react-native'
 import { generateId } from './hash'
 
 const styles = StyleSheet.create({
   input: {
     padding: 20,
-    borderBottomColor: '#bbb',
+    borderBottomColor: 'rgba(20, 20, 20, 0.04)',
     borderBottomWidth: 1,
     fontSize: 24
+  },
+  switchView: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 10,
+    paddingTop: 10,
+    borderBottomColor: 'rgba(20, 20, 20, 0.4)',
+    borderBottomWidth: 1
   }
 })
 
 export class CreateItem extends React.Component {
   state = {
-    newItemText: ''
+    newItemText: '',
+    showLocationToggle: false,
+    recordLocation: true
   }
 
   addItem = () => {
@@ -40,19 +54,42 @@ export class CreateItem extends React.Component {
     this.setState(prev => ({ ...prev, newItemText: text }))
   }
 
-  render () {
-    const { newItem } = this.state
+  showLocationToggle = () => {
+    this.setState(() => ({ showLocationToggle: true }))
+  }
 
+  hideLocationToggle = () => {
+    this.setState(() => ({ showLocationToggle: false }))
+  }
+
+  toggleRecordLocation = newValue => {
+    this.setState(() => ({ recordLocation: newValue }))
+  }
+
+  render () {
+    const { newItemText, showLocationToggle, recordLocation } = this.state
     return (
-      <TextInput
-        style={styles.input}
-        autoCorrect={false}
-        placeholder='Add something!'
-        returnKeyType='done'
-        onChangeText={this.storeNewItemText}
-        onSubmitEditing={() => this.addItem()}
-        value={newItem}
-      />
+      <View>
+        <TextInput
+          style={styles.input}
+          autoCorrect={false}
+          placeholder='Add something!'
+          returnKeyType='done'
+          onChangeText={this.storeNewItemText}
+          onFocus={this.showLocationToggle}
+          onBlur={this.hideLocationToggle}
+          onSubmitEditing={() => this.addItem()}
+          value={newItemText}
+        />
+        {showLocationToggle &&
+          <View style={styles.switchView}>
+            <Text>Tag your current location?</Text>
+            <Switch
+              value={recordLocation}
+              onValueChange={this.toggleRecordLocation}
+            />
+          </View>}
+      </View>
     )
   }
 }
