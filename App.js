592e3b522dc8432eb8d686a2c48ff853b216083a
dotfold <james@dotfold.io>
2018-08-10 23:23:55 -0700

    feat(request current geo position on boot):

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { Home } from './app/home'
 import { List } from './app/list'
 import { ListNavigator } from './app/list_navigator'
 
-// load data from AsyncStorage
+// request current position on boot to trigger permissions request
+navigator.geolocation.getCurrentPosition(s => {
+  // noop
+})
 
-// then move to home
 import { createStackNavigator } from 'react-navigation'
 
 const App = createStackNavigator(
