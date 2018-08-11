3c797430c576a382b0801a734866e3a225ec14de
dotfold <james@dotfold.io>
2018-08-10 21:57:42 -0700

    feat(card_item): styles - bigger text

--- app/card_item.js
+++ app/card_item.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    fontSize: 24
+  }
+})
 
 const CardItem = ({ title }) => {
   return (
     <View>
-      <Text>{title}</Text>
+      <Text style={styles.text}>{title}</Text>
     </View>
   )
 }
