aa38ee9a69a06ce62bf9dda0d02dee42c180261f
dotfold <james@dotfold.io>
2018-08-10 21:58:28 -0700

    feat(home): add an icon to the cards for some contrast. proper heading text

--- app/home.js
+++ app/home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
 import { CardItem } from './card_item'
 import { Card } from './card'
@@ -8,40 +9,85 @@ import { Card } from './card'
 // that propagates new dimensions from a change event
 const { height, width } = Dimensions.get('window')
 const styles = StyleSheet.create({
-  container: {
+  outer: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingTop: 30
+  },
+  container: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-start'
   },
+  titlesView: {
+    flex: 0,
+    justifyContent: 'flex-start',
+    paddingLeft: 15,
+    paddingBottom: 30,
+    paddingTop: 20
+  },
   title: {
-    fontSize: 36,
-    marginTop: 60,
-    marginBottom: 30,
+    fontSize: 30,
+    marginTop: 10,
+    marginBottom: 10,
+    fontWeight: '300'
+  },
+  tagline: {
+    fontSize: 20,
+    marginTop: 0,
+    marginBottom: 0,
     fontWeight: '300'
   },
   card: {
     backgroundColor: '#ccc',
     padding: 20,
-    // flex: 1,
+    flex: 1,
     width: width - 25,
     height: 185,
     borderRadius: 10
+  },
+  icon: {
+    opacity: 0.2,
+    position: 'absolute',
+    right: 10,
+    top: -10,
+    maxHeight: 195
   }
 })
 
 class Home extends React.Component {
+  static navigationOptions = { title: '', header: null }
   render () {
     const { navigation } = this.props
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Welcome Home!</Text>
-        <Card>
-          <CardItem title='Things you can do now!' />
-        </Card>
-        <Card onTouch={() => navigation.navigate('List')}>
-          <CardItem title='List items for home' />
-        </Card>
+      <View style={styles.outer}>
+        <View style={styles.titlesView}>
+          <Text style={styles.title}>Hey there!</Text>
+          <Text style={styles.tagline}>Looking for something to do?</Text>
+        </View>
+        <View style={styles.container}>
+          <Card>
+            <CardItem title='{x} items tagged at your current location' />
+            <View style={styles.icon}>
+              <Ionicons
+                name='ios-locate'
+                size={242}
+                color='rgba(255, 255, 255, 0.7)'
+              />
+            </View>
+          </Card>
+          <Card onTouch={() => navigation.navigate('List')}>
+            <CardItem title='All your items' />
+            <View style={styles.icon}>
+              <Ionicons
+                name='ios-list'
+                size={242}
+                style={{ right: 55 }}
+                color='rgba(255, 255, 255, 0.7)'
+              />
+            </View>
+          </Card>
+        </View>
       </View>
     )
   }
